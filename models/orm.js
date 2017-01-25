2744514eb35ad64db64b6cc910d920e6cbc2c539
Terence Ro <terencero@Terences-MacBook-Air.local>
2017-01-25 14:18:13 -0600

    complete orm object and export; set up server.js

--- models/orm.js
+++ models/orm.js
@@ -23,5 +23,51 @@ function objToSql(obj) {
 }
 
 var orm = {
-	selectAll:
-}
+	selectAll: function(tableInput, cb) {
+		var queryString = 'SELECT * FROM ' + tableInput + ';';
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+	insertOne: function(table, cols, vals, cb) {
+		var queryString = 'INSERT INTO ' + table;
+
+		queryString += ' (';
+		queryString += cols.toString();
+		queryString += ')';
+		queryString += 'VALUES (';
+		queryString += questionMarks(vals.length);
+		queryString += ')';
+
+		console.log(queryString);
+
+		connection.query(queryString, vals, function(err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	},
+	updateOne: function(table, objColVals, condition, cb) {
+		var queryString = 'UPDATE ' + table;
+
+		queryString += ' SET';
+		queryString += objToSql(objColVals);
+		queryString += ' WHERE ';
+		queryString += condition;
+
+		console.log(queryString);
+		connection.query(queryString, function (err, result) {
+			if (err) {
+				throw err;
+			}
+			cb(result);
+		});
+	}
+};
+
+// Export the orm object to the model (burger.js)
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-var mysql = require('mysql');
-var express = require('express')
+var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var exphdbs = require('express-handlbars');
+// Set handlebars
+var exphbs = require('express-handlebars');
+
+var port  = 3306;
+var app = express();
+
+// Serve/route to static content
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Import routes and the server access to them
+var routes = require('./controllers/burger_controller.js');
+
+app.use('/', routes);
+
+app.listen(port);
+
