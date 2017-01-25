509b00ad7f89d38ab20c787462d36b78735ce236
Terence Ro <terencero@Terences-MacBook-Air.local>
2017-01-24 23:22:33 -0600

    update database

---
+++ server.js
@@ -0,0 +1,7 @@
+var mysql = require('mysql');
+var express = require('express')
+var path = require('path');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var exphdbs = require('express-handlbars');
