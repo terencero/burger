3074c097d0a21b5d7a1d8758ea6bc30fe6218f23
Terence Ro <terencero@dhcp-10-102-167-100.wireless.northwestern.private>
2017-01-27 09:55:27 -0600

    moved orm file; updated burger.js file with orm calls

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,5 +1,15 @@
-var express = require('express')
+var express = require('express');
 
 var burger = require('../models/burger.js');
 
 var router = express.Router();
+
+router.get('/', function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
