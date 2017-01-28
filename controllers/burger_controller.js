f3be79ee78865b790b256ec86d29ba535fe88208
Terence Ro <terencero@Terences-MacBook-Air.local>
2017-01-27 19:04:01 -0600

    complete burger js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,11 +5,29 @@ var burger = require('../models/burger.js');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	burger.all(function(data) {
+	burger.selectAll(function(data) {
 		var hbsObject = {
 			burgers: data
 		};
 		console.log(hbsObject);
 		res.render('index', hbsObject);
+	});
+});
+
+router.post('/', function(req, res) {
+	burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function() {
+		res.redirect('/');
+	});
+});
+
+router.put('/:id', function(req, res) {
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	burger.updateOne({
+		devoured: req.body.devoured
+	}, condition, function() {
+		res.redirect('/');
 	});
 });
--- models/burger.js
+++ models/burger.js
@@ -18,3 +18,4 @@ var burger = {
 	}
 };
 
+module.exports = burger;
